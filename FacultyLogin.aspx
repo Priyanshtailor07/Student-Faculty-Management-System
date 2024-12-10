41df1a9273fbc54781cac13a3d09e9704a0af358
Priyanshu Luharia <144381155+Priyanshtailor07@users.noreply.github.com>
2024-12-10 22:07:58 +0530

    Faculty login page

---
+++ FacultyLogin.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FacultyLogin.aspx.cs" Inherits="StudentFacultyManagementSystem.FacultyLogin" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Faculty Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            width: 300px;
+            margin: 100px auto;
+            text-align: center;
+        }
+        .form-group {
+            margin-bottom: 15px;
+
+        }
+        input[type="text"], input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            margin-top: 5px;
+        }
+        button {
+            width: 100%;
+            padding: 10px;
+            background-color: #28a745;
+            color: white;
+            border: none;
+        }
+        button:hover {
+            background-color: #218838;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Faculty Login</h2>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" placeholder="Enter email"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" TextMode="Password" placeholder="Enter password"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FacultyLogin.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace StudentFacultyManagementSystem
+{
+    public partial class FacultyLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                Response.Write("<script>alert('Please enter email and password.');</script>");
+                return;
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["StudentFacultyDatabase"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "SELECT * FROM Faculty WHERE Email = @Email AND Password = @Password";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    Session["FacultyID"] = reader["FacultyID"].ToString();
+                    Session["FacultyName"] = reader["FirstName"].ToString();
+                    Response.Redirect("FacultyHome.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid email or password.');</script>");
+                }
+            }
+        }
+    }
+}
