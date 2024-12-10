04baef68fdc3919a402a2d7aa5f78b48c6329d55
Priyanshu Luharia <144381155+Priyanshtailor07@users.noreply.github.com>
2024-12-10 22:15:15 +0530

    Student register page

---
+++ StudentRegister.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentRegister.aspx.cs" Inherits="StudentFacultyManagementSystem.StudentRegister" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Registration</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Student Registration</h2>
+            <div class="form-group">
+                <label>First Name</label>
+                <asp:TextBox ID="txtFirstName" runat="server" placeholder="First Name"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Last Name</label>
+                <asp:TextBox ID="txtLastName" runat="server" placeholder="Last Name"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <asp:TextBox ID="txtEmail" runat="server" placeholder="Email"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" placeholder="Password"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Phone Number</label>
+                <asp:TextBox ID="txtPhoneNumber" runat="server" placeholder="Phone Number"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Department</label>
+                <asp:TextBox ID="txtDepartment" runat="server" placeholder="Department"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Button ID="btnRegister" runat="server" Text="Register" OnClick="btnRegister_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudentRegister.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StudentFacultyManagementSystem
+{
+    public partial class StudentRegister : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text.Trim();
+            string lastName = txtLastName.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            string phoneNumber = txtPhoneNumber.Text.Trim();
+            string department = txtDepartment.Text.Trim();
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) ||
+                string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                Response.Write("<script>alert('Please fill all the required fields.');</script>");
+                return;
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["StudentFacultyDatabase"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "INSERT INTO Students (FirstName, LastName, Email, Password, PhoneNumber, Department, EnrollmentDate) " +
+                               "VALUES (@FirstName, @LastName, @Email, @Password, @PhoneNumber, @Department, @EnrollmentDate)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                cmd.Parameters.AddWithValue("@Department", department);
+                cmd.Parameters.AddWithValue("@EnrollmentDate", DateTime.Now);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    Response.Write("<script>alert('Registration successful. Please log in.');</script>");
+                    Response.Redirect("StudentLogin.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Registration failed. Please try again.');</script>");
+                }
+            }
+        }
+    }
+}
