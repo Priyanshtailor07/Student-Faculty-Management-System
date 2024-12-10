84bd2a43aa8c45ceb4da0db859dc85c462114b08
Priyanshu Luharia <144381155+Priyanshtailor07@users.noreply.github.com>
2024-12-10 22:12:16 +0530

    Admin student page

---
+++ AdminStudent.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.Master" AutoEventWireup="true" CodeBehind="AdminStudent.aspx.cs" Inherits="StudentFacultyManagementSystem.AdminStudent" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .btn-danger {
+            background-color: #ff4d4d;
+            color: white;
+            border: none;
+            padding: 5px 10px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+        .btn-danger:hover {
+            background-color: #ff1a1a;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Student Details</h2>
+    <asp:GridView ID="gvStudents" runat="server" AutoGenerateColumns="False" OnRowCommand="gvStudents_RowCommand" CssClass="table table-striped">
+    <Columns>
+        <asp:BoundField DataField="StudentID" HeaderText="Student ID" />
+        <asp:BoundField DataField="FullName" HeaderText="Full Name" />
+        <asp:BoundField DataField="Department" HeaderText="Department" />
+        <asp:BoundField DataField="Email" HeaderText="Email" />
+        <asp:BoundField DataField="CourseName" HeaderText="Course Enrolled" />
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:Button ID="btnDelete" runat="server" Text="Delete" CommandName="DeleteStudent" CommandArgument='<%# Eval("StudentID") %>' CssClass="btn btn-danger" />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+
+</asp:Content>
---
+++ AdminStudent.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentFacultyManagementSystem
+{
+    public partial class AdminStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                BindStudentsGrid();
+            }
+        }
+
+        private void BindStudentsGrid()
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["StudentFacultyDatabase"].ConnectionString;
+
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT 
+                s.StudentID, 
+                CONCAT(s.FirstName, ' ', s.LastName) AS FullName,
+                s.Department, 
+                s.Email, 
+                c.CourseName
+            FROM Students s
+            LEFT JOIN StudentCourses sc ON s.StudentID = sc.StudentID
+            LEFT JOIN Courses c ON sc.CourseID = c.CourseID";
+
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    gvStudents.DataSource = dt;
+                    gvStudents.DataBind();
+                }
+            }
+        }
+
+        protected void gvStudents_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "DeleteStudent")
+            {
+                if (e.CommandArgument != null)
+                {
+                    int studentID = Convert.ToInt32(e.CommandArgument);
+                    string connectionString = WebConfigurationManager.ConnectionStrings["StudentFacultyDatabase"].ConnectionString;
+
+                    if (connectionString == null)
+                    {
+                        throw new Exception("Connection string is not defined in web.config");
+                    }
+
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        // First, delete the student from the StudentCourses table
+                        string deleteCoursesQuery = "DELETE FROM StudentCourses WHERE StudentID = @StudentID";
+                        using (SqlCommand cmd = new SqlCommand(deleteCoursesQuery, con))
+                        {
+                            cmd.Parameters.AddWithValue("@StudentID", studentID);
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        // Now, delete the student from the Students table
+                        string deleteStudentQuery = "DELETE FROM Students WHERE StudentID = @StudentID";
+                        using (SqlCommand cmd = new SqlCommand(deleteStudentQuery, con))
+                        {
+                            cmd.Parameters.AddWithValue("@StudentID", studentID);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    // Rebind the GridView to reflect changes
+                    BindStudentsGrid();
+                }
+            }
+        }
+
+    }
+}
